9cf657818ad48322841a4613710983322c4a0e77
Jay-G2017 <jay.gao@139.com>
2017-10-09 10:00:51 +0800

    restaurant and review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@review = Review.new(review_params)
+		@review.restaurant = @restaurant
+		if @review.save
+			redirect_to restaurant_path(@restaurant)
+		else
+			render restaurant_path(@restaurant) 
+		end
+	end
+
+	private
+	def review_params
+		params.require(:review).permit(:content, :rating)
+	end
+end
